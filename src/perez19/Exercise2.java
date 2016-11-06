c4a50425069da5c42d1d1b66b089da9d061dc13e
mperez5 <mperez154@gmail.com>
2016-11-05 21:54:53 -0400

    Added new files for exercise 2

--- src/perez19/Exercise2.java
+++ src/perez19/Exercise2.java
@@ -2,47 +2,38 @@ package perez19;
 
 import java.util.ArrayList;
 
-import com.sun.javafx.geom.Rectangle;
-
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
 
 public class Exercise2 {
 
 	public static void main(String[] args) {
-			
-		
 		
 		ArrayList<Object> list = new ArrayList<>();
+				
+		list.add(new Rectangle(25,25));
+		list.add(new Circle(25));
+		list.add("James Gosling");
+		list.add(12);
 		
-		list.add(new Rectangle(25, 25, 25, 25));
-		list.add(new Circle(50));
-		list.add("Test");
-		list.add(5);
+		Rectangle rectangle = new Rectangle(25,25);
 		
-		displayList(list);
-
+		System.out.print("Circle area is ");
+		//System.out.println(Exercise2.<Rectangle>displayList(rectangle));
+		
+		
 	}
 	
-	//Referenced page 743 for help with this
-	public static <E> void displayList(ArrayList<Object> list)
-	{
-		for(int i = 0; i < list.size(); i++)
-		{
-			list.get(i).toString();
-		}
+	//See MyGenericMethod demo for sample output
+	public static <E extends GeometricObject> double displayList(E shape)
+	{			
+		return shape.getArea();	
 	}
-
 }
 
 /*
- *READ PAGES 739 - 745 FOR HELP WITH THIS
- *
- * EXERCISE 2 Create an ArrayList of Objects. Add the following objects to the
- * list in any order: 病 Rectangle 病 Circle 病 String 病n Integer
- * 
  * Write and call a void method that takes the list as its sole argument:
  * 頻rocess the list with a loop 品nside the loop, display the areas of both the
  * Circle and Rectangle objects but run toString() for the String and Integer.
- * 
  * SAMPLE OUTPUT Area is 50.0 James Gosling 12 Area is 95.03317777109125
  */
